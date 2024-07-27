349ccfb8a8b0f40bf7dad13776fb9925b2992878
Diiboi <118180436+Diiboi@users.noreply.github.com>
2024-07-27 14:08:10 +0200

    installation of tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
+import "./globals.css";
 function App() {
-
-  return (
-   <div>
-    Welcome to BNPL E-Commerce
-   </div>
-  )
+  return <div className="text-black capitalize py-9 text-center">Welcome to BNPL E-Commerce</div>;
 }
 
-export default App
+export default App;
